3f4afc7addbae185fce8478cfeb9c1087ae33304
Knokia <nokiaa74@mail.ru>
2021-11-19 06:46:07 +0300

    Xddd

---
+++ Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿
+namespace RoflanKurs
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timerLeft = new System.Windows.Forms.Timer(this.components);
+            this.timerRight = new System.Windows.Forms.Timer(this.components);
+            this.timerUp = new System.Windows.Forms.Timer(this.components);
+            this.timerDown = new System.Windows.Forms.Timer(this.components);
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.MainSpaceShip = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.MainSpaceShip)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timerLeft
+            // 
+            this.timerLeft.Interval = 5;
+            this.timerLeft.Tick += new System.EventHandler(this.timerLeft_Tick);
+            // 
+            // timerRight
+            // 
+            this.timerRight.Interval = 5;
+            this.timerRight.Tick += new System.EventHandler(this.timerRight_Tick);
+            // 
+            // timerUp
+            // 
+            this.timerUp.Interval = 5;
+            this.timerUp.Tick += new System.EventHandler(this.timerUp_Tick);
+            // 
+            // timerDown
+            // 
+            this.timerDown.Interval = 10;
+            this.timerDown.Tick += new System.EventHandler(this.timerDown_Tick);
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 10;
+            this.timer.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // MainSpaceShip
+            // 
+            this.MainSpaceShip.BackColor = System.Drawing.Color.Transparent;
+            this.MainSpaceShip.Image = global::RoflanKurs.Properties.Resources.kosmolet;
+            this.MainSpaceShip.Location = new System.Drawing.Point(509, 338);
+            this.MainSpaceShip.Name = "MainSpaceShip";
+            this.MainSpaceShip.Size = new System.Drawing.Size(167, 210);
+            this.MainSpaceShip.TabIndex = 0;
+            this.MainSpaceShip.TabStop = false;
+            this.MainSpaceShip.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::RoflanKurs.Properties.Resources.BackGround__1_;
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.MainSpaceShip);
+            this.MaximumSize = new System.Drawing.Size(1280, 720);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.MainSpaceShip)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.PictureBox MainSpaceShip;
+        private System.Windows.Forms.Timer timerLeft;
+        private System.Windows.Forms.Timer timerRight;
+        private System.Windows.Forms.Timer timerUp;
+        private System.Windows.Forms.Timer timerDown;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RoflanKurs
+{
+    public partial class Form1 : Form
+    {
+        int SpaceShipSpeed;
+        PictureBox[] Missile;
+        int SpeedOfMissile;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SpaceShipSpeed = 10;
+            Missile = new PictureBox[2];
+            SpeedOfMissile = 40;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timerLeft_Tick(object sender, EventArgs e)
+        {
+            if(MainSpaceShip.Left > 10)
+            {
+                MainSpaceShip.Left -= SpaceShipSpeed;
+            }
+        }
+
+        private void timerRight_Tick(object sender, EventArgs e)
+        {
+            if (MainSpaceShip.Left < 1150)
+            {
+                MainSpaceShip.Left += SpaceShipSpeed;
+            }
+        }
+
+        private void timerUp_Tick(object sender, EventArgs e)
+        {
+            MainSpaceShip.Top -= SpaceShipSpeed;
+        }
+
+        private void timerDown_Tick(object sender, EventArgs e)
+        {
+            MainSpaceShip.Top += SpaceShipSpeed;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Left)
+            {
+                timerLeft.Start();
+            }
+
+            if (e.KeyCode == Keys.Right)
+            {
+                timerRight.Start();
+            }
+
+            if (e.KeyCode == Keys.Up)
+            {
+                timerUp.Start();
+            }
+
+            if (e.KeyCode == Keys.Down)
+            {
+                timerDown.Start();
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            timerLeft.Stop();
+            timerRight.Stop();
+            timerUp.Stop();
+            timerDown.Stop();
+        }
+
+        
+    }
+}
